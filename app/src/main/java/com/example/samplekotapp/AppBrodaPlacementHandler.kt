e0a710bae0fb1874bfdc76397f2cc8fd61364e71
Anil Kumar MR <107169398+anil-mr@users.noreply.github.com>
2023-09-12 14:23:24 +0530

    utility class update

--- app/src/main/java/com/example/samplekotapp/AppBrodaPlacementHandler.kt
+++ app/src/main/java/com/example/samplekotapp/AppBrodaPlacementHandler.kt
@@ -1,49 +1,36 @@
 package com.example.samplekotapp
 
-import android.annotation.SuppressLint
-import android.content.Context
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 
 object AppBrodaPlacementHandler {
-    @SuppressLint("StaticFieldLeak")
-    private var firebaseRemoteConfig: FirebaseRemoteConfig? = null;
-
-    fun initRemoteConfigAndSavePlacements(context: Context) {
+    fun initRemoteConfigAndSavePlacements() {
         val mFirebaseRemoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
         mFirebaseRemoteConfig.setConfigSettingsAsync(remoteConfigSettings {
             minimumFetchIntervalInSeconds = 0
         })
-        setFirebaseRemoteConfig(mFirebaseRemoteConfig)
         mFirebaseRemoteConfig.fetchAndActivate()
     }
 
-    fun fetchAndSavePlacements(mFirebaseRemoteConfig: FirebaseRemoteConfig, context: Context) {
-        mFirebaseRemoteConfig.fetchAndActivate()
-      setFirebaseRemoteConfig(mFirebaseRemoteConfig)
+    fun fetchAndSavePlacements() {
+        Firebase.remoteConfig.fetchAndActivate()
     }
 
     fun loadPlacements(key: String): Array<String> {
-        val value = firebaseRemoteConfig?.getString(key);
-        if (value === "") {
-            return arrayOf()
-        }
-        val placement = convertToArray(value)
-        return placement ?: arrayOf()
+        val value = FirebaseRemoteConfig.getInstance().getString(key)
+        if (value.isEmpty()) return arrayOf()
+        return convertToArray(value)
     }
 
     private fun convertToArray(value: String?): Array<String> {
-        val newValue = value!!.substring(1, value.length - 1)
-        val array: Array<String> = newValue.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        if(value.isNullOrEmpty()) return arrayOf()
+        val array: Array<String> = value.substring(1, value.length - 1).split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
         for (i in array.indices) {
             array[i] = array[i].trim { it <= ' ' }.replace("^\"|\"$".toRegex(), "")
         }
         return array
     }
 
-    private fun setFirebaseRemoteConfig (remoteConfig:FirebaseRemoteConfig){
-        firebaseRemoteConfig = remoteConfig
-    }
 }
